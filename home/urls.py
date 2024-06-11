6101b21233e70419cced33e3c97a5d0fe34c7a3e
Aaron Storey <aaron.storey@hotmail.co.uk>
2024-06-11 20:51:08 +0000

    Feat(home) home now shows content from the models

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomeContent.as_view, name="home"),
+    path("", views.index, name="home"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render
 from django.contrib import admin
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
-from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.views.generic import ListView
 from .models import HomeNews
 
-class HomeContent(generic.ListView):
-    queryset = HomeNews.objects.all()
-    template_name = "home/index.html"
+#class HomeContent(generic.ListView):
+#    queryset = HomeNews.objects.all()
+#    template_name = "home/index.html"
 
-    def index(request):
-        """
-        view to show template
-        """
-        queryset = HomeNews.objects.all()
-        viewbag = {"contents": content,
-                "title": title}
+def index(request):
+    """
+    view to show template
+    """
+    queryset = HomeNews.objects.all()
+    content = queryset
+    viewbag = {"contents": content}
         
-        return render (request, "home/index.html",viewbag)
+    return render (request, "home/index.html",viewbag)
