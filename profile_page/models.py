d57cb0aa0363bd9d361a8bfa177fb107cc577c4f
Aaron Storey <aaron.storey@hotmail.co.uk>
2024-07-04 20:05:02 +0000

    Feat(user.model)i have added a model for user sessions that store the name and then all the exercises will be added underneath

--- profile_page/models.py
+++ profile_page/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import User
 from ptsessions.models import PtSessions
 
@@ -9,7 +8,7 @@ class Profile(models.Model):
     #user image
     username = User.username
     email = User.email
-    date_of_birth = models.DateField()
+    date_of_birth = models.DateField(default= "0/00/0000")
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.username
--- userprograms/models.py
+++ userprograms/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class MainUserProgram(models.Model):
+    BODY_PART = (
+        ('Legs', 'Legs'),
+        ('Chest', 'Chest'),
+        ('Back', 'Back'),
+        ('Abs', 'Abs'),
+        ('Shoulders', 'Shoulders'),
+        ('All Body', 'All Body')
+    )
+
+    session_name = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body_part = models.CharField(max_length=40, choices=BODY_PART, default='All Body')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.session_name} | Made by {self.user}"
+
+class UserPrograme(models.Model):
+    session = models.ForeignKey(MainUserProgram, on_delete=models.CASCADE)
+    reps = models.IntegerField()
+    sets = models.IntegerField()
+    weight = models.IntegerField()
+    comment = models.TextField(max_length=300)
+    def __str__(self):
+        return f"{self.reps} x {self.sets} x {self.weight} | Comments {self.comment}"
+
+    
