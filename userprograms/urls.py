06a044153ecf067cdfbad78f869882329bc1d980
Aaron Storey <aaron.storey@hotmail.co.uk>
2024-07-07 17:22:06 +0000

    Feat(usersessions) the users session template is working and i have got the model to show on the page

--- userprograms/urls.py
+++ userprograms/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("userprograms/", views.index, name="userprograms")
+    path("userprograms/", views.index, name="userprograms"),
+    # test
+    #path('userprograms/<int:id>/', views.mysessions, name='mysessions'),
 ]
--- userprograms/views.py
+++ userprograms/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import MainUserProgram, UserPrograme
 
 def index(request):
     """
     view to show template
     """
-    #queryset = HomeNews.objects.all()
-    #content = queryset
-    #viewbag = {"contents": content}
+    queryset = MainUserProgram.objects.all()
+    content = queryset
+    viewbag = {"contents": content}
         
-    return render (request, "userprograms/index.html")
+    return render (request, "userprograms/index.html",viewbag)
+
+
+
+def mysessions(request, id):
+    """
+    View to show template
+    """
+    session = get_object_or_404(MainUserProgram, id=id)  # i need to get the id to work
+    usersessions_form = UserSessionsForm(request.POST or None)  
+
+    if request.method == "POST":
+        if usersessions_form.is_valid():
+            user_program = usersessions_form.save(commit=False)  
+            user_program.user = request.user  
+            user_program.post = session
+            user_program.save()
+            messages.success(request, 'Comment Is Just Going To Be Checked')
+
+    return render(request, "userprograms/index.html", {
+        'main_program': session,
+        'usersessions_form': usersessions_form,
+    })
