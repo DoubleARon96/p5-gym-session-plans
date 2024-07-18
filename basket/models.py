e3e6803e382465dcfa8bd6bf1e53609db3cf6ef7
Aaron Storey <aaron.storey@hotmail.co.uk>
2024-07-18 18:52:05 +0000

    Feat(basket) this is the basket data base made

--- basket/models.py
+++ basket/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 from ptsessions.models import PtSessions
 # Create your models here.
+
+class BasketItem(models.Model):
+    """
+    This will make the basket line item
+    """
+    basket = models.ForeignKey('Basket', on_delete=models.CASCADE)
+    product = models.ForeignKey(PtSessions, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.product.name}"
+    
+class Basket(models.Model):
+    """
+    This makes the data for the total and all products added to it
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(PtSessions, through='BasketItem')
+
+    def __str__(self):
+        return f"Basket for {self.user.username}"
