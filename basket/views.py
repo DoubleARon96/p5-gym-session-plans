cb601fdf05bf45d2f7045479f37bfacd13db931d
Aaron Storey <aaron.storey@hotmail.co.uk>
2024-07-18 19:01:16 +0000

    Feat(basket views) the views are set up on basket now

--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404,redirect,reverse
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from .models import BasketItem, Basket
 from ptsessions.models import PtSessions
 #from .models import 
 #from .forms import 
@@ -8,7 +9,9 @@ from ptsessions.models import PtSessions
 # Create your views here.
 def Basket (request):
     
-    #items = request.session.get('basket',{})
+    items = Basket.objects.all()
+    itemlines = get_object_or_404(BasketItem)
     template = 'basket/index.html'
-    #content = { }
-    return render(request, template)
+    content = {'items':items,
+               'itemlines':itemlines }
+    return render(request, template,content)
