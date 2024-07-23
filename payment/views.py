8ca2ec64cd52f11afa721271233bd012c44def78
Aaron Storey <aaron.storey@hotmail.co.uk>
2024-07-23 20:43:03 +0000

    Feat(payment) i have added the basket page to the payment as a place marker and now trying to get the form to show

--- payment/views.py
+++ payment/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from basket.models import BasketItem, Basket
-
 from .forms import OrderForm
 
 # Create your views here.
 
 def Checkout (request):
-    items = request.session.get(' Basket',{})
+    items = request.session.get('Basket',{})
     if not items:
         messages.error(request, "You have a Empty Basket")
-        return redirect(reverse('basket'))
+        #return redirect(reverse('basket'))
     order_form = OrderForm()
-    template = 'payment/index.html'
+    template = 'payments/index.html'
     content = {
-        'order_form': order_form
+        'order_form': order_form,
+        'basket': items,
     }
     return render(request, template, content)
