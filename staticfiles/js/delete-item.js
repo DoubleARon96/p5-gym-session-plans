15f1b4699eb01759a29a7011b34ad67aae34f1f9
Aaron Storey <aaron.storey@hotmail.co.uk>
2024-07-25 20:10:01 +0000

    Feat(static) i have just collected the statics to push to heroku

---
+++ staticfiles/js/delete-item.js
@@ -0,0 +1,11 @@
+const deleteButtons = document.getElementsByClassName("btn-delete");
+var myModal = new bootstrap.Modal(document.getElementById('deleteModal'));
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+for (let button of deleteButtons) {
+  button.addEventListener("click", (a) => {
+    let basket_item_id = a.currentTarget.dataset.basket_item_id;
+    deleteConfirm.href = `basket/delete_item/${basket_item_id}`;
+    myModal.show();
+  });
+}
